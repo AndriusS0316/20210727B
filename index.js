676f5bdb178d93610d72d6ab473ca6c5a4a8e6fa
Andrius <andriuss0316@gmail.com>
2021-08-01 21:14:37 +0300

    20210801

--- index.js
+++ index.js
@@ -15,5 +15,9 @@ let data2 = "";
 try {
     data1 += await fs.readFile("pirmas.json");
     data2 += await fs.readFile("antras.json");
-    
+
+} catch (err) {
+    console.log("Klaida bandant perskaityti failo turini. Siuo metu, failas laikinai yra nepasiekiamas arba jis buvo perkeltas i kita vieta. Dar karta patikrinkite faila. ", err);
 }
+console.log(data1 + data2);
+console.log("pabaiga");
