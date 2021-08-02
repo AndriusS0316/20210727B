b0aa87b683104b3c4ed04e126137f99920e1114b
Andrius <andriuss0316@gmail.com>
2021-08-02 15:29:13 +0300

    20210802

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-import * as fs from "fs";
-console.log("start");
-function readFile(name) {
-    return new Promise((resolve, reject) => {
-        fs.readFile(name, (err, data) => {
-            if (err) {
-                return reject(err);
-            }
-            resolve(data);
+import { Server } from "net";
+
+let server = new Server(socket => {
+    console.log("kazkas prisijunge");
+    console.log(socket.localAddress, socket.localPort, socket.remoteAdress, socket.remotePort);
+    socket.setEncoding("utf-8");
+    let allData = "";
+    socket.on("data", data => {
+        allData += data;
+    });
+    socket.on("end", () => {
+        console.log(allData);
+        console.log("---------------");
+        let resp = "HTTP/1.1 200 OK\r\n\r\n";
+        let html = "<html><body><h1>Hello world !!!</h1></body></html>"
+        console.log(resp + html + "\r\n\r\n");
+        socket.write( resp + html + "\r\n\r\n", () => {
+            socket.end();
         });
     });
-}
-// let data = "" + await readFile("a.txt");
-// console.log(data);
-// console.log("finish");
-readFile("a.txt").then(data => {
-    console.log("" + data);
-    console.log("finish");
+    
 });
+
+server.listen(3000);
+
