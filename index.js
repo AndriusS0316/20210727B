087db5bd8dfa40a191965a1848eae046f61c233a
Andrius <andriuss0316@gmail.com>
2021-08-03 14:29:16 +0300

    20210803

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
 import { Server } from "net";
+import * as fs from "fs/promises"; 
+
+
+const WEB_DIR = "web";
 
 let server = new Server((socket) => {
-    console.log("kazkas prisijunge");
-    console.log(
-        socket.localAddress,
-        socket.localPort,
-        socket.remoteAddress,
-        socket.remotePort,
-    );
+    // console.log("kazkas prisijunge");
+    // console.log(
+    //     socket.localAddress,
+    //     socket.localPort,
+    //     socket.remoteAddress,
+    //     socket.remotePort,
+    // );
     socket.setEncoding("utf-8");
     let allData = "";
     socket.on("data", (data) => {
         allData += data;
         let lines = allData.split("\r\n");
         if (lines.findIndex(e => e === "") !== -1) {
-            console.log(allData);
+            let path = lines[0].split(" ")[1];
+            console.log(path);
+            let fileName = WEB_DIR + path;
+            // web/index.html
+            // 
+
+            //console.log(allData);
             console.log("-----------");
             let resp = "HTTP/1.1 200 Ok\r\n\r\n";
             let html = "<html><body><h1>labas pasauli !!!</h1></body></html>";
