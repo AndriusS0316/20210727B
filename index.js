297808f7d5438bcf3fb0c11d80147da5425981cd
Andrius <andriuss0316@gmail.com>
2021-08-03 15:40:05 +0300

    20210803

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { Server } from "net";
-import * as fs from "fs/promises"; 
-
+import * as fs from "fs/promises";
+import * as path from "path";
 
 const WEB_DIR = "web";
 
@@ -14,25 +14,39 @@ let server = new Server((socket) => {
     // );
     socket.setEncoding("utf-8");
     let allData = "";
-    socket.on("data", (data) => {
+    socket.on("data", async (data) => {
         allData += data;
         let lines = allData.split("\r\n");
         if (lines.findIndex(e => e === "") !== -1) {
-            let path = lines[0].split(" ")[1];
-            console.log(path);
-            let fileName = WEB_DIR + path;
-            // web/index.html
-            // 
-
-            //console.log(allData);
-            console.log("-----------");
-            let resp = "HTTP/1.1 200 Ok\r\n\r\n";
-            let html = "<html><body><h1>labas pasauli !!!</h1></body></html>";
-            console.log(resp + html + "\r\n\r\n");
-            console.log("=============");
-            socket.write(resp + html + "\r\n\r\n", () => {
+            let fileName = lines[0].split(" ")[1];
+            let realFile = path.join(WEB_DIR, fileName);
+            console.log(realFile);
+            let resp;
+            try {
+                resp = "HTTP/1.1 200 OK\r\n\r\n";
+                const stats = await fs.stat(readFile);
+                let html = await fs.readFile(realFile, {
+                encoding: "utf-8"
+                });
+                resp += html;
+            } catch (err) {
+                rsp = "HTTP/1.1 404 Not Found\r\n\r\n";
+            }
+            socket.write(resp + "\r\n\r\n", () => {
                 socket.end();
             });
+            
+            // console.log(fileName, realFile);
+            // web/index.html - unix (obuolys)
+            // web\index.html - win
+
+            //console.log(allData);
+            // console.log("-----------");
+            // let resp = "HTTP/1.1 200 Ok\r\n\r\n";
+            // let html = "<html><body><h1>labas pasauli !!!</h1></body></html>";
+            // console.log(resp + html + "\r\n\r\n");
+            // console.log("=============");
+            
         }
     });
 });
